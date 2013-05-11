8d6376ebdc236b39bae678e6e291e682919ff5ac
shikakun <k@u-n.jp>
2013-05-11 13:17:51 +0900

    Autopager 対応した

--- javascripts/js.js
+++ javascripts/js.js
@@ -1,3 +1,10 @@
 $(document).ready(function() {
+  //$(".pager.pager_next").find("a").addClass("autopager");
+  //var ap = $(".pager.pager_next").find("a").selector;
+  //alert(ap);
   
+  $.autopager({
+    content: '#entries',
+    link: '#autopager'
+  });
 });
