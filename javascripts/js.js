88bddd009351f681b36a36dedb74c524db4b889e
shikakun <k@u-n.jp>
2013-05-21 21:00:00 +0900

    全ページを読み込んだら loading 表示しないようにした

--- javascripts/js.js
+++ javascripts/js.js
@@ -1,6 +1,15 @@
-$(document).ready(function() {  
+$(document).ready(function() {
+  var page_now = 0;
+  var page_num = $('#page_num').text();
+  
   $.autopager({
     content: '#entries',
-    link: '#page_next'
+    link: '#page_next',
+    load: function() {
+      page_now = $(this).find('#page_now').text();
+      if( page_now == page_num ){
+        $('#page').hide();
+      }
+    }
   });
 });
